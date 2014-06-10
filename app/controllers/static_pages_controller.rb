82db4c747aa1ee9ddf5809697024a41c47cc48b6
Shohei SUNAGAWA <kanpe777@gmail.com>
2014-06-10 15:46:44 +0900

    Implements easy model
    ```
    $ vim app/controllers/static_pages_controller.rb
    $ vim app/views/static_pages/index.html.erb
    $ vim config/routes.rb
    ```

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,21 @@
 class StaticPagesController < ApplicationController
   def index
+    @dates = MyDate.all
+  end
+
+  def add
+    date = Time.now.to_s
+    MyDate.create(org_value: date)
+    redirect_to action: :index
+  end
+
+  def destroy
+    MyDate.find(params[:id]).delete
+    redirect_to action: :index
+  end
+
+  def clear
+    MyDate.delete_all
+    redirect_to action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  post   '/add',     to: 'static_pages#add'
+  delete '/destroy/:id', to: 'static_pages#destroy'
+  delete '/clear',      to: 'static_pages#clear'
 end
